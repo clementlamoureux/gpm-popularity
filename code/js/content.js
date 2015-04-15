a4150e16ed345e652e4af2eee2d6b06307700ce8
Clément LAMOUREUX <clement@raccourci.fr>
2015-04-15 14:31:07 +0200

    Display improvement

--- code/js/content.js
+++ code/js/content.js
@@ -30,7 +30,9 @@ var apply = function(numberOfEl){
 			result = Math.ceil(result) * 1.5;
 			var render = '<span class="point" style="width:' + result + 'px">' + value.value + '</span>';
 			$('.song-row').each(function(key, valueA){
-				if($(valueA).find('[data-col="index"]').text()===value.track){
+				if($(valueA).find('[data-col="title"]').text()===value.title &&
+					$(valueA).find('[data-col="album"]').text()===value.album &&
+					$(valueA).find('[data-col="artist"]').text()===value.artist){
 					if(!$(valueA).find('[data-col="note"]').length) {
 						$(valueA).find('[data-col="title"]').prepend('<div data-col="note">' + render + '</div>');
 					}
@@ -39,36 +41,43 @@ var apply = function(numberOfEl){
 			
 		});
 		watchClicks()
+		$('button.button[data-id="lf"]').html(values.length);
 	}
 }
 var fetchAll =  _.debounce(function(){
-	values = [];
 	var numberOfEl = $('.song-row').length;
 	$('.song-row').each(function( key, value ) {
 		var title =  jQuery(value).find('[data-col="title"]').find('.content').clone().children().remove().end().text();
-		var track =  jQuery(value).find('[data-col="index"]').find('.content').clone().children().remove().end().text();
+		var album =  jQuery(value).find('[data-col="album"]').find('.content').find('.text').text();
 		var artist =  jQuery(value).find('[data-col="artist"]').find('.text').clone().children().remove().end().text();
-		$.get("https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=453b53a3e4a54335490c26d32a071201&artist=" + artist + "&track=" + title + "&format=json", function(data) {
-			if(data && data.track){
-				values.push(
-				{
-					id: artist+title,
-					track: track,
-					value: data.track.playcount,
-					element: value
+		if(_.findIndex(values, {title: title, artist:artist,album:album}) === -1){
+			$.get("https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=453b53a3e4a54335490c26d32a071201&artist=" + artist + "&track=" + title + "&format=json", function(data) {
+				if(data && data.track){
+					values.push(
+					{
+						artist: artist,
+						album: album,
+						title: title,
+						value: data.track.playcount,
+						element: value
+					}
+					);
+				}else{
+					values.push(
+					{
+						artist: artist,
+						album: album,
+						title: title,
+						value: 0,
+						element: value
+					}
+					);
 				}
-				);
-			}else{
-				values.push(
-				{
-					id: artist+title,
-					value: 0,
-					element: value
-				}
-				);
-			}
+				apply(numberOfEl);
+			});
+		}else{
 			apply(numberOfEl);
-		})
+		}
 	});
 
 }, 100, {leading: false, trailing: true});
